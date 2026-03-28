65c1885f13ba99ea69f4fea5a3c9bd74a9a37c28
Jounaxis <jgs123098@gmail.com>
2026-03-27 22:54:51 -0300

    Alterações feitas com uso do Swagger para consulta e melhor descrição das requisições

--- src/main/java/br/com/fiap/api_rest/ApiRestApplication.java
+++ src/main/java/br/com/fiap/api_rest/ApiRestApplication.java
@@ -1,9 +1,19 @@
 package br.com.fiap.api_rest;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info =
+@Info(title = "Api de Produtos",
+		version = "v1",
+		description = "API RESTful de Produtos com Swagger e Validation",
+		license = @License(name = "MIT", identifier = "MIT"),
+		contact = @Contact(name = "RM560907", email = "rm560907@fiap.com.br")))
 public class ApiRestApplication {
 
 	public static void main(String[] args) {
