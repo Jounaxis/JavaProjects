4742a17e7f94811c55c3532f8392b1410b77a591
Jounaxis <jgs123098@gmail.com>
2026-05-06 22:19:16 -0300

    Adicionando arquivos de aulas perdidas, git pull na branch master para conseguir acompanhar o conteudo

---
+++ src/main/java/br/com/fiap/api_rest/security/SecurityConfigurations.java
@@ -0,0 +1,13 @@
+package br.com.fiap.api_rest.security;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+
+    }
+}
---
+++ src/main/java/br/com/fiap/api_rest/service/AuthService.java
@@ -0,0 +1,9 @@
+package br.com.fiap.api_rest.service;
+
+import br.com.fiap.api_rest.repository.ProdutoRepository;
+
+public class AuthService implements UserDetailsService {
+    private final UsuarioRepository usuarioRepository;
+
+    public AuthService(Usuario)
+}
